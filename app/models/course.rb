3a4f434084514bb965bda7b2fa5e486808a5b0cf
Cole Meurer <cole.meurer@gmail.com>
2017-07-17 14:09:11 +0900

    added associations and dependencies: fixed pluralities

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
-  has_many :curators, through: :curatorship, source: :user
-  has_many :students, through: :enrollment, source: :user
+  has_many :curators, through: :curatorships, source: :user
+  has_many :students, through: :enrollments, source: :user
   has_many :books, dependent: :destroy
   has_many :curatorships, dependent: :destroy
   has_many :enrollments, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :courses_curated, through: :curatorship, source: :course
-  has_many :courses_taken, through: :enrollment, source: :course
+  has_many :courses_curated, through: :curatorships, source: :course
+  has_many :courses_taken, through: :enrollments, source: :course
   has_many :posts, dependent: :nullify
   has_many :curatorships, dependent: :nullify
   has_many :enrollments:, dependent: :destroy

810c9ec562b2e9939009069cceecf256662d9ce6
Owen Klumb <owen.klumb@gmail.com>
2017-07-18 14:33:08 +0900

    Javascript starting to work on book page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,24 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+ //     $(document).ready(function(){
+	// 	 $(document).bind('keypress',pressed);
+	// 	    var userSelection;
+	// 	    if (window.getSelection) {
+	// 	        userSelection = window.getSelection();
+	// 	    }
+	// 	    else if (document.selection) { // should come last; Opera!
+	// 	        userSelection = document.selection.createRange();
+	// 	    }
+		    
+	// 	  function pressed(e)
+	// 	    {
+	// 	    if(e.keyCode === 13)
+	// 	    {
+	// 	        alert(userSelection);
+	// 	        //put button.click() here
+	// 	    }
+	// 	}
+	// }); 

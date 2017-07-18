b461f49fa5d0c9196f55b1e05e7ece5f058cefe0
Cole Meurer <cole.meurer@gmail.com>
2017-07-18 10:26:08 +0900

    routes functional

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  namespace :dashboard do
+  scope '/dashboard' do
     resources :courses do
       resources :books, except: [:show]
     end

64bf65954648f04a175a98a7580bf83ad870a2d5
Cole Meurer <cole.meurer@gmail.com>
2017-07-21 10:57:07 +0900

    preapred enrollments controller

--- app/controllers/curatorships_controller.rb
+++ app/controllers/curatorships_controller.rb
@@ -1,17 +1,36 @@
 class CuratorshipsController < ApplicationController
   # TODO: add authorization
+  before_action :set_course_id
   skip_after_action :verify_authorized
   skip_after_action :verify_policy_scoped
 
   def index
+    @curatorships = Curatorship.all
   end
 
   def new
+    @curatorship = Curatorship.new
   end
 
   def create
+    @curatorship = Curatorship.new(curatorship_params)
+    @curatorship.course = @course
+
+    if @curatorship.save
+      redirect_to
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def set_course_id
+    @course = Course.find(:course_id)
+  end
+
+  def curatorship_params
+    params.require(:curatorship).permit(:user_id)
   end
 end
--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,17 +1,36 @@
 class EnrollmentsController < ApplicationController
   # TODO: add authorization
+  before_action :set_course_id
   skip_after_action :verify_authorized
   skip_after_action :verify_policy_scoped
 
   def index
+    @enrollments = Enrollment.all
   end
 
   def new
+    @enrollment = Enrollment.new
   end
 
   def create
+    @enrollment = Enrollment.new(enrollment_params)
+    @enrollment.course = @course
+
+    if @enrollment.save
+      redirect_to
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def set_course_id
+    @course = Course.find(:course_id)
+  end
+
+  def enrollment_params
+    params.require(:enrollment).permit(:user_id)
   end
 end

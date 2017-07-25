f5243d754e2cd82258c1f171230ef7f53fd6eb40
Cole Meurer <cole.meurer@gmail.com>
2017-07-25 17:56:17 +0900

    navbar and user account page use users avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -13,11 +13,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png)
+    %w(jpg jpeg svg png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:

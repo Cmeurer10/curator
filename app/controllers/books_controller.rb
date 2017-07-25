0ee1edf7cca30266dafd7887a3e22d67f106da5e
Owen Klumb <owen.klumb@gmail.com>
2017-07-25 19:27:39 +0900

    Hover/click now works for current user conversations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -80,6 +80,10 @@ class BooksController < ApplicationController
     end
   end
 
+  def refreshConvos
+    render :partial => "/books/sidebar/conversations" 
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book

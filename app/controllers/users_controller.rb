d3da569398e2a856c9f3bc05006706554803bc34
Cole Meurer <cole.meurer@gmail.com>
2017-07-26 15:05:49 +0900

    added authorization for inviting to curators only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
-  skip_after_action :verify_authorized
   before_action :set_course
 
   def new
     @user = User.new
+    authorize @user
   end
 
   def invite
     users = User.where(email: user_params[:email])
     user = users.empty? ? User.new : users.first
+    authorize user
     user.email = user_params[:email]
     user.invite!
     enrollment = Enrollment.new

24812e7c23f8263204f583d7262ca06cfb203cd6
Cole Meurer <cole.meurer@gmail.com>
2017-07-27 11:01:09 +0900

    added upvoters to posts

---
+++ db/migrate/20170727015859_add_upvoters_to_posts.rb
@@ -0,0 +1,5 @@
+class AddUpvotersToPosts < ActiveRecord::Migration[5.0]
+  def change
+    add_column :posts, :upvoters, :string, array: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726021250) do
+ActiveRecord::Schema.define(version: 20170727015859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170726021250) do
     t.integer  "user_id"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "upvoters",                                     array: true
     t.index ["conversation_id"], name: "index_posts_on_conversation_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
@@ -110,6 +111,7 @@ ActiveRecord::Schema.define(version: 20170726021250) do
     t.string   "invited_by_type"
     t.integer  "invited_by_id"
     t.integer  "invitations_count",      default: 0
+    t.string   "upvoted",                                                 array: true
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
     t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
